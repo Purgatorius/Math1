74f60e390a208ff9bc21a8ca24537af277ecd482
Purgatorius <Bartlomiej.Bugala@gmail.com>
2017-06-06 17:30:29 +0200

    Create code_Math1.java

---
+++ code_Math1.java
@@ -0,0 +1,16 @@
+/**
+ * Created by B4RT on 2017-06-06.
+ */
+public class Main {
+    public static void main (String[] args)
+    {
+        int z1 = 15;
+        int z2 = 5;
+
+        System.out.println("15+5 = " +(z1+z2));
+        System.out.println("15-5 = " +(z1-z2));
+        System.out.println("15*5 = " +(z1*z2));
+        System.out.println("15/5 = " +(z1/z2));
+        System.out.println("15%5 = " +(z1%z2));
+    }
+}
